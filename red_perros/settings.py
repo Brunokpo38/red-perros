113374273f901d9344410665c665a97c0591552e
Bruno <bruno@gmail.com>
2025-04-30 15:52:19 -0300

    Configuración de base de datos para Render

--- red_perros/settings.py
+++ red_perros/settings.py
@@ -74,14 +74,16 @@ WSGI_APPLICATION = 'red_perros.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'perros_datos',
-        'USER': 'perros_datos_user',
-        'PASSWORD': 'vPOBphLckrXw5tjEQwgB1mjh3mnAsDA3',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': dj_database_url.config(
+        default='postgresql://perros_datos_user:vPOBphLckrXw5tjEQwgB1mjh3mnAsDA3@dpg-d096cevfte5s73dqjkmg-a/perros_datos,
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
     }
 }
 
